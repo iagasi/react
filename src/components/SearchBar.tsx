dd60bf3100c2e109424adda19cdb7d5dcdb62853
iagasi <nameagasi@gmail.com>
2023-03-20 02:15:57 +0400

    fix:save to local storage

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,9 +5,11 @@ type state = {
   searchText: string;
 };
 export class SearchBar extends React.Component {
+  input: React.LegacyRef<HTMLInputElement> | undefined;
   constructor(props: state) {
     super(props);
     this.add = this.add.bind(this);
+    this.input = React.createRef();
   }
   state = {
     searchText: '',
@@ -19,17 +21,23 @@ export class SearchBar extends React.Component {
       searchText: data || '',
     });
   }
+  componentDidUpdate() {
+    //LocalStorage.set(this.input.current.value)
+    LocalStorage.set(this.state.searchText);
+  }
   add(e: React.ChangeEvent<HTMLInputElement>) {
     this.setState(() => ({
       searchText: e.target.value,
     }));
-    LocalStorage.set(e.target.value);
+
+    // LocalStorage.set(e.target.value);
   }
   render() {
     return (
       <div className="search" data-testid="test-search">
         <img className="search__icon" src="/search.png" alt="" />
         <input
+          ref={this.input}
           className="search__input"
           type="text"
           value={this.state.searchText}
