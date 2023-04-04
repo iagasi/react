04605005664348dddb63c14c733a3f606fca4886
iagasi <nameagasi@gmail.com>
2023-04-04 18:02:07 +0400

    fix : in test linter error

--- src/tests/Form.test.tsx
+++ src/tests/Form.test.tsx
@@ -1,13 +1,8 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Form } from '../components/form/Form';
 import React from 'react';
-import { vi } from 'vitest';
 
 describe('Form', () => {
-  const onSubmit = vi.fn((e) => {
-    e.preventDefault();
-  });
-
   it('Renders Children', () => {
     render(<Form></Form>);
     expect(screen.getByRole('form')).toBeInTheDocument();
